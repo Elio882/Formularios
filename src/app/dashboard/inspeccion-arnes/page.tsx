30597d1fa79b7231ca7e8e9fb987e0576b249ef8
hackathon2024 <mfloresherbas00@gmail.com>
2025-02-02 15:21:06 -0400

    added firm digital

--- src/app/dashboard/inspeccion-arnes/page.tsx
+++ src/app/dashboard/inspeccion-arnes/page.tsx
@@ -12,6 +12,8 @@ import {
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Image from "next/image";
+import SignatureCanvas from "react-signature-canvas";
+import { useRef } from "react";
 
 interface FormData {
   superintendencia: string;
@@ -24,14 +26,41 @@ interface FormData {
   fecha: string;
   observaciones: string;
   resultados: Record<string, string>;
+  firma: string;
 }
 
-const items = [
-  "Amortiguador de caídas - Desgaste del protector",
-  "Amortiguador de caídas - Testigo activado",
-  "Tejido trenzado - Fibras rotas",
-  "CONECTORES - Ganchos de seguridad dañados",
-  "Hebillas - Corrosión de la hebilla",
+const sections = [
+  {
+    title: "ARGOLLAS EN 'D' O ANILLOS",
+    items: [
+      "Con deformaciones o desgaste excesivo (dobladura, etc.)",
+      "Picaduras, grietas, trizaduras (que abarquen un 50% de una sección)",
+      "Corrosión de la argolla (Corrosión de toda la argolla)",
+    ],
+  },
+  {
+    title: "PROTECTOR DE ESPALDA",
+    items: [
+      "Cortes (que pasan todo el grosor de la pieza)",
+      "Deterioro por uso o calor (reseco)",
+    ],
+  },
+  {
+    title: "HEBILLAS",
+    items: [
+      "Desgaste excesivo o deformaciones (dobladuras, etc.)",
+      "Picaduras, grietas, trizaduras, quemaduras (que abarquen un 50% de una sección)",
+      "Corrosión de las hebillas (Corrosión de toda la hebilla)",
+      "Defecto de funcionamiento (no enganchan o se traban)",
+    ],
+  },
+  {
+    title: "PASADORES",
+    items: [
+      "Cortes (que pasan todo el grosor de la pieza)",
+      "Deterioro por uso o calor (reseco)",
+    ],
+  },
 ];
 
 export default function page() {
@@ -40,9 +69,20 @@ export default function page() {
     control,
     formState: { errors },
   } = useForm<FormData>();
+  const sigCanvasRef = useRef<SignatureCanvas | null>(null);
 
   const onSubmit = (data: FormData) => {
+    if (sigCanvasRef.current) {
+      const signature = sigCanvasRef.current
+        .getTrimmedCanvas()
+        .toDataURL("image/png");
+      data.firma = signature;
+    }
     console.log("Datos enviados:", data);
+  };
+
+  const clearSignature = () => {
+    sigCanvasRef.current?.clear();
   };
   return (
     <Box
@@ -74,7 +114,7 @@ export default function page() {
             />
           </Grid>
           <Grid
-            size={{ xs: 2, sm: 6, md: 8 }}
+            size={{ xs: 8, sm: 6, md: 8 }}
             sx={{
               backgroundColor: "#f0f0f0",
               border: "2px solid #000",
@@ -237,64 +277,116 @@ export default function page() {
           </Grid>
 
           {/* Preguntas del checklist */}
-          {items.map((item, index) => (
-            <Box key={index} sx={{ mt: 2 }}>
-              <Typography>{item}</Typography>
-              <Controller
-                name={`resultados.${item}` as const}
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <RadioGroup {...field} row>
-                    <FormControlLabel
-                      value="SI"
-                      control={<Radio />}
-                      label="Sí"
-                    />
-                    <FormControlLabel
-                      value="NO"
-                      control={<Radio />}
-                      label="No"
-                    />
-                    <FormControlLabel
-                      value="NA"
-                      control={<Radio />}
-                      label="No aplica"
-                    />
-                  </RadioGroup>
-                )}
-              />
-            </Box>
-          ))}
-          {/* Observaciones */}
-          <Controller
-            name="observaciones"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Observaciones"
-                multiline
-                rows={4}
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
+          <Box sx={{ width: "100%" }}>
+            <Typography align="center" sx={{ textTransform: "uppercase" }}>
+              arnes <br />
+              HERRAJES(Componentes palsticos y metalicos integrales del arnes)
+            </Typography>
+            {sections.map((section, sectionIndex) => (
+              <Box key={sectionIndex} sx={{ mt: 4, width: "100%" }}>
+                <Typography variant="h6" gutterBottom>
+                  {section.title}
+                </Typography>
+                {section.items.map((item, index) => (
+                  <Grid
+                    container
+                    spacing={2}
+                    alignItems="center"
+                    key={index}
+                    sx={{ mt: 2 }}
+                  >
+                    <Grid size={{ xs: 4 }}>
+                      <Typography>{item}</Typography>
+                    </Grid>
+                    <Grid size={{ xs: 4 }}>
+                      <Controller
+                        name={
+                          `resultados.${section.title}.${item}.respuesta` as const
+                        }
+                        control={control}
+                        render={({ field }) => (
+                          <RadioGroup {...field} row>
+                            <FormControlLabel
+                              value="SI"
+                              control={<Radio />}
+                              label="Sí"
+                            />
+                            <FormControlLabel
+                              value="NO"
+                              control={<Radio />}
+                              label="No"
+                            />
+                            <FormControlLabel
+                              value="NA"
+                              control={<Radio />}
+                              label="No aplica"
+                            />
+                          </RadioGroup>
+                        )}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 4 }}>
+                      <Controller
+                        name={
+                          `resultados.${section.title}.${item}.observacion` as const
+                        }
+                        control={control}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            label="Observaciones"
+                            fullWidth
+                            size="small"
+                          />
+                        )}
+                      />
+                    </Grid>
+                  </Grid>
+                ))}
+              </Box>
+            ))}
+          </Box>
         </Grid>
+        {/* Campo de firma */}
+        <Box sx={{ mt: 4 }} alignItems={"center"}>
+          <Typography variant="h6" gutterBottom>
+            Firma Digital
+          </Typography>
+          <Box
+            sx={{
+              border: "1px solid #ccc",
+              borderRadius: 2,
+              width: "100%",
+              height: 200,
+              mb: 2,
+            }}
+          >
+            <SignatureCanvas
+              ref={sigCanvasRef}
+              canvasProps={{
+                width: 760,
+                height: 200,
+                className: "sigCanvas",
+              }}
+            />
+          </Box>
+          <Button onClick={clearSignature} variant="outlined" color="secondary">
+            Limpiar Firma
+          </Button>
+        </Box>
+      </Grid>
 
-        {/* Botón de enviar */}
+      {/* Botón de enviar */}
+      <center>
         <Button
           type="submit"
           variant="contained"
           color="primary"
-          fullWidth
-          sx={{ mt: 2 }}
+          sx={{ mt: 2, width: "25%" }}
         >
           Enviar
         </Button>
-      </Grid>
+      </center>
     </Box>
   );
 }
